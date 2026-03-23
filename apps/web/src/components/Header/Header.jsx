7429f6b084133706b1afcf7dedd03a0289c4b16f
smd-1 <skdanish1999@gmail.com>
2026-03-23 19:24:14 +0530

    feat(search): Show the search option in product and invoice page only

--- apps/web/src/components/Header/Header.jsx
+++ apps/web/src/components/Header/Header.jsx
@@ -1,26 +1,32 @@
 import "./Header.css";
 import { useHeader } from "../../contexts/header-context";
 import { Search, Settings } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const { title } = useHeader();
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const showSearch = location.pathname === "/product" || location.pathname === "/invoice";
+
   return (
     <div className="header">
       <div className="header-mobile-logo">
         <img src="/logo.svg" alt="Company Logo" />
       </div>
       <p className="header-title">{title}</p>
-      <div className="search">
-        <Search
-          width={24}
-          height={24}
-          color="#fff"
-          style={{ marginLeft: "1.5rem" }}
-        />
-        <input type="text" placeholder="Search here..." />
-      </div>
+      {showSearch && (
+        <div className="search">
+          <Search
+            width={24}
+            height={24}
+            color="#fff"
+            style={{ marginLeft: "1.5rem" }}
+          />
+          <input type="text" placeholder="Search here..." />
+        </div>
+      )}
       <div className="header-mobile-settings">
         <Settings onClick={() => navigate("/settings")} width={24} height={24} color="#fff" />
       </div>
